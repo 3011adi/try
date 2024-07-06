ae70f57dd3dda53c9ca4ce5a5185583a451f0ecb
Adi Naik <161313416+3011adi@users.noreply.github.com>
2024-07-06 14:32:34 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+export const PORT =5555;
+
+export const mangoDBURL =
+ 'mongodb+srv://3011adinaik:toqkiG-rexji7-kexbis@item-store.2gzwbsj.mongodb.net/Item-Store?retryWrites=true&w=majority&appName=Item-Store'
