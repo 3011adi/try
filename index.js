b76376637080164832c93e52c2b101827ad57da7
Adi Naik <161313416+3011adi@users.noreply.github.com>
2024-07-06 16:48:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,16 +38,32 @@ app.post('/books',async(request,response) => {
 
 });
 
+app.get('/books',async(request,response) => {
+   try{
+    const books =await Book.find({});
+    return response.status(200).json({
+        count: books.length,
+        data: books});
+   }
+   catch(error)
+   {
+    console.log(error.message);
+    response.status(500).send({message:error.message});
+   }
+});
+
 mongoose
 .connect(mangoDBURL)
 .then( () =>{
    console.log('connected');
-   app.listen(PORT, () =>{
-    console.log(`appmis : ${PORT}`);
-
-});
+  
 })
 
 .catch( (error) =>{
     console.log(error);
 })
+
+ app.listen(PORT, () =>{
+    console.log(`appmis : ${PORT}`);
+
+});
